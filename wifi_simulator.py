315612334a0d2e226e1e0ce64aaa11040ab9736e
Underemployed <underemployed871@gmail.com>
2025-01-20 16:42:23 +0530

    working demo version

--- wifi_simulator.py
+++ wifi_simulator.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 import time
+import random
 
 app = Flask(__name__)
 CORS(app)
@@ -12,13 +13,12 @@ last_toggle = time.time()
 def get_pad_state():
     global pad_state, last_toggle
     
-    # Toggle state every 5 seconds for testing
     current_time = time.time()
-    if current_time - last_toggle > 5:
+    if current_time - last_toggle > random.uniform(3, 7):
         pad_state = 1 if pad_state == 0 else 0
         last_toggle = current_time
     
     return str(pad_state)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000,debug=True)
